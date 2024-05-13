305f1bb1e00b60e9935a69065abc674152652758
Franzie Yoogan <franzieyoogan@gmail.com>
2024-05-13 18:46:30 -0300

    animatioooon

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,4 +9,48 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  eventOver() {
+
+    const subtitle:any = document.getElementById('subtitle')
+    const iconStyle:any = document.getElementById('iconStyle')
+
+    subtitle.style.visibility = "visible"
+    iconStyle.style.border = "1px solid purple"
+
+    setTimeout(() => {
+
+       subtitle.style.left = "9em"
+ 
+      subtitle.style.transition = "1s"
+
+      subtitle.style.opacity = "1"
+      
+    }, 100);
+
+  
+
+
+  }
+
+  eventOut() {
+
+    const subtitle:any = document.getElementById('subtitle')
+    const iconStyle:any = document.getElementById('iconStyle')
+
+    
+       subtitle.style.left = "6em"
+ 
+      subtitle.style.transition = "1s"
+
+      setTimeout(() => {
+
+        subtitle.style.opacity = "0"
+        
+      }, 100);
+
+      
+
+
+  }
+
 }
