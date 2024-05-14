e3dd905da0de16f7ef01a1873d0d261bd33795f8
Franzie Yoogan <franzieyoogan@gmail.com>
2024-05-13 21:24:21 -0300

    chatBotBeginning

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -13,8 +13,50 @@ export class ChatComponent implements OnInit {
   ngOnInit(): void {
     
     const dateBubble:any = document.getElementById('dateBubble')
+    const message:any = document.getElementById('message')
+    const containerItemBubble:any = document.getElementById('containerItemBubble')
+    const pStyle:any = document.getElementById('pStyle')
     const date = new Date()
     dateBubble.innerHTML = date.toString().slice(0,25)
+
+    pStyle.innerHTML = "i'm Ghostie your little friend. How's it going ?-?"
+
+    message.addEventListener('input', (event: { target: { value: string; }; }) => {
+
+      if(event.target.value == "fine") {
+
+        containerItemBubble.style.marginBottom = "3em"
+        containerItemBubble.style.transition = "1s"
+        containerItemBubble.style.opacity = "0"
+
+        setTimeout(() => {
+          
+          containerItemBubble.style.marginBottom = "0em"
+          containerItemBubble.style.transition = "1s"
+          containerItemBubble.style.opacity = "1"
+          pStyle.innerHTML = "that's so sad, you wanna see a video ?-? maybe u gon feel better(1 YES, 2 NO)"
+        }, 1000);
+
+      }
+
+      if(event.target.value == "good") {
+
+        containerItemBubble.style.marginBottom = "3em"
+        containerItemBubble.style.transition = "1s"
+        containerItemBubble.style.opacity = "0"
+        
+        setTimeout(() => {
+          
+          containerItemBubble.style.marginBottom = "0em"
+          containerItemBubble.style.transition = "1s"
+          containerItemBubble.style.opacity = "1"
+          pStyle.innerHTML = "that's so sad, you wanna see a video ?-? maybe u gon feel better(1 YES, 2 NO)"
+
+        }, 1000);
+
+      }
+
+    })
 
   }
 
